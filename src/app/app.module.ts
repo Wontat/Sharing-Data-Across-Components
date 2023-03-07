fbb501cd459213a96fcf4009a137c454ac66d493
Wontat <98503728+Wontat@users.noreply.github.com>
2023-03-07 19:36:12 +0800

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { EmployeeModule } from './employee/employee.module';
+
+import { ProductModule } from './product/product.module';
+import { SharedService } from './shared.service';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule,
+    EmployeeModule,
+    ProductModule,
+  ],
+  declarations: [AppComponent, HelloComponent],
+  bootstrap: [AppComponent],
+  providers: [SharedService],
+})
+export class AppModule {}
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService, Employee } from '../shared.service';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css'],
+})
+export class EmployeeComponent implements OnInit {
+  employees: Employee[];
+
+  constructor(private sharedService: SharedService) {}
+
+  ngOnInit() {
+    this.employees = this.sharedService.employees;
+  }
+}
